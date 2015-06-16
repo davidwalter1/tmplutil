81fe9dcbe839d3c5982599603589450d1aaaf4ba
David Walter <walter.david@gmail.com>
2015-06-16 10:47:31 -0500

    Add cajun wiki markup conversion and improved logging methods

--- common.go
+++ common.go
@@ -11,8 +11,66 @@ import (
     "bufio"
     "net/http"
     "regexp"
+    "io"
+    "log"
+	"os"
 	wiki     "github.com/m4tty/cajun"
 )
+
+var debug = false
+
+var (
+    Trace   *log.Logger
+    Info    *log.Logger
+    Warning *log.Logger
+    Error   *log.Logger
+)
+
+func Init(
+    traceHandle io.Writer,
+    infoHandle io.Writer,
+    warningHandle io.Writer,
+    errorHandle io.Writer) {
+
+    Trace = log.New(traceHandle,
+        "TRACE: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Info = log.New(infoHandle,
+        "INFO: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Warning = log.New(warningHandle,
+        "WARNING: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Error = log.New(errorHandle,
+        "ERROR: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func LogString( r* http.Request ) string {
+    return fmt.Sprintf("\"%s %s %s\" \"%s\" \"%s\"",
+        r.Method,
+        r.URL.String(),
+        r.Proto,
+        r.Referer(),
+        r.UserAgent() )
+}
+
+func init() {
+    flag.Parse()
+	Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+}
+
+// valid path are names to process as markdown markup with template
+// var WikiPath    = regexp.MustCompile("^/(wiki)/([-\\._:a-zA-Z0-9]+\\.wiki)$")
+// var MdPath      = regexp.MustCompile("^/(slides|menu1|menu2|menu3|plain|test)/([-\\._:a-zA-Z0-9]+\\.md)$")
+// var WikiPathRegex    		= regexp.MustCompile("^/(wiki)/(.+\\.wiki)(\\?.*)?$")
+// var MdPathRegex      		= regexp.MustCompile("^/(slides|menu1|menu2|menu3|plain|test)/(.+\\.md)(\\?.*)?$")
+var FilePathRegex    		= regexp.MustCompile("^(/(slides|menu1|menu2|menu3|plain|test|wiki)/)?(.+\\.md|.+\\.wiki|.+\\.html?)(\\?.*)*$")
+var DirectoryListingTemplateFile = "directory-listing.html"
+var DirectoryListingTemplateText = Load( DirectoryListingTemplateFile )
 
 var titleSplit              = "title:"
 var subtitleSplit           = "subtitle:"
@@ -26,9 +84,9 @@ var h2SplitStar             = "** "
 var h1SplitHash             = "# "   
 var h2SplitHash             = "## "   
 
-var templateHead            = flag.String( "templateHead",      "head.thtml",            	"header filename")
-var templateBody            = flag.String( "templateBody",      "body.thtml",            	"golang formatted template filename")
-var templateFoot            = flag.String( "templateFoot",      "foot.thtml",            	"footer filename")
+var templateHead            = flag.String( "templateHead",      "head.html",            	"header filename")
+var templateBody            = flag.String( "templateBody",      "body.html",            	"golang formatted template filename")
+var templateFoot            = flag.String( "templateFoot",      "foot.html",            	"footer filename")
 var Filename                = flag.String( "filename",          "",                      	"markdown filename")
 var RawFlag                 = flag.Bool  ( "raw",               false,                   	"markup a markdown filename -filename write to stdout with defaults and exit.")
 var Slides                  = flag.Bool  ( "slides",            false,                   	"markup a markdown filename -filename write to stdout with slide format and exit.")
@@ -151,9 +209,6 @@ func Load( filename string ) ( *string ) {
 }
 
 func LoadPages( filename string ) ( *Pages ) {
-    if len( filename ) < 3 || filename[len( filename )-3:] != ".md" {
-        filename = filename + ".md"
-    }
     text := Load( filename )
     if text == nil {
         return nil
@@ -204,10 +259,12 @@ func IsSet( text string, search string, out* bool, found* bool ) {
 }
 
 func IsMarkdown( filename string ) bool {
+    if debug { fmt.Println( "len( filename ) >= 3 && filename[len( filename )-3:] == \".md\"",  len( filename ) >= 3 && filename[len( filename )-3:] == ".md" ) }
     return len( filename ) >= 3 && filename[len( filename )-3:] == ".md"
 }
 
 func IsWiki( filename string ) bool {
+    if debug { fmt.Println( "len( filename ) >= 5 && filename[len( filename )-5:] == \".wiki\"",  len( filename ) >= 5 && filename[len( filename )-5:] == ".wiki" ) }
     return len( filename ) >= 5 && filename[len( filename )-5:] == ".wiki"
 }
 
@@ -240,9 +297,6 @@ var Fmap = template.FuncMap {
 
 func MarkupMarkdown( filename string, args Arglist, raw bool ) string {
     if raw {
-        if len( filename ) < 3 || filename[len( filename )-3:] != ".md" {
-            filename = filename + ".md"
-        }
         text := Load( filename )
         pages := strings.Split( string( *text ), "---" ) 
         html  := ""
@@ -255,7 +309,11 @@ func MarkupMarkdown( filename string, args Arglist, raw bool ) string {
                 string( blackfriday.MarkdownCommon( []byte( html  ) ) ) +
                 "\n</body>\n</html>\n"
         } else { return "" }
-	} else if len( filename ) >= 5 && filename[ len( filename ) - 5: ] == ".wiki" {
+	}
+
+	if debug { fmt.Println( "MarkupMarkdown not raw",  filename ) }
+	if IsWiki( filename ) { // len( filename ) >= 5 && filename[ len( filename ) - 5: ] == ".wiki" {
+		fmt.Println( "MarkupMarkdown wiki",  filename )
         text := Load( filename )
 		if output, err := wiki.Transform( *text ); err == nil {
 			return output
@@ -264,9 +322,11 @@ func MarkupMarkdown( filename string, args Arglist, raw bool ) string {
 		}
 	}
 
-    Head         := Load( *args.TemplateName + "-head.thtml" )
-    Body         := Load( *args.TemplateName + "-body.thtml" )
-    Foot         := Load( *args.TemplateName + "-foot.thtml" )
+	if debug { fmt.Println( "MarkupMarkdown not wiki",  filename ) }
+
+    Head         := Load( *args.TemplateName + "-head.html" )
+    Body         := Load( *args.TemplateName + "-body.html" )
+    Foot         := Load( *args.TemplateName + "-foot.html" )
 
     if Body != nil && Head != nil && Foot != nil {
         runner, err := template.New("tmplutil").Funcs( Fmap ).Parse( *Body )
@@ -295,40 +355,38 @@ func MarkupMarkdown( filename string, args Arglist, raw bool ) string {
     return ""
 }
 
-
-// valid path are names to process as markdown markup with template
-var WikiPath    = regexp.MustCompile("^/(wiki)/([\\.-_:a-zA-Z0-9]+.wiki)$")
-var MdPath      = regexp.MustCompile("^/(slides|menu1|menu2|menu3|plain|test)/([\\.-_:a-zA-Z0-9]+.md)$")
-var DirectoryListingTemplateFile = "directory-listing.thtml"
-var DirectoryListingTemplateText = Load( DirectoryListingTemplateFile )
-
 func MakeHandler( fn func( http.ResponseWriter, *http.Request, string, string, Arglist ), args Arglist ) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
-        Log( r )
-        if len( r.URL.Path ) > 3 && r.URL.Path[ len( r.URL.Path ) - 3: ] == ".md" {
-			m := MdPath.FindStringSubmatch(r.URL.Path)
-            if m == nil {
-                http.FileServer( http.Dir( *args.WebRoot ) )
-                return
-            }
-            args.Title = & m[2]
-            fn( w, r, m[1], m[2], args )
-        } else if len( r.URL.Path ) >= 5 && r.URL.Path[ len( r.URL.Path ) - 5: ] == ".wiki" {
-            m := WikiPath.FindStringSubmatch(r.URL.Path)
-            if m == nil {
-                http.FileServer( http.Dir( *args.WebRoot ) )
-                return
-            }
-            args.Title = & m[2]
-			WikiHandler( w, r, m[1], m[2], args )
+		Info.Printf( fmt.Sprintf( "Host %-20s Client %-20s URL.Path [%s] %s\n", r.Host, r.RemoteAddr, r.URL.Path, LogString( r ) ) )
+        patharray := FilePathRegex.FindStringSubmatch( r.URL.Path )
+		if patharray == nil {
+			http.FileServer( http.Dir( *args.WebRoot ) )
+			return
+		}
+        if patharray != nil && len( patharray ) > 3 {
+			path, filename := patharray[2], patharray[3]
+			if debug { fmt.Println( r.URL.Path, patharray, "filename", filename ) }
+            args.Title = & filename
+            fn( w, r, path, filename, args )
+        // } else if len( r.URL.Path ) >= 5 && r.URL.Path[ len( r.URL.Path ) - 5: ] == ".wiki" {
+        //     // m := WikiPathRegex.FindStringSubmatch( r.URL.Path )
+		// 	m := patharray
+		// 	if debug { fmt.Println( r.URL.Path, m ) }
+        //     if m == nil {
+        //         http.FileServer( http.Dir( *args.WebRoot ) )
+        //         return
+        //     }
+        //     args.Title = & m[2]
+		// 	WikiHandler( w, r, m[1], m[2], args )
         } else {
-            m := MdPath.FindStringSubmatch(r.URL.Path)
-            if m == nil {
-                http.FileServer( http.Dir( *args.WebRoot ) )
-                return
-            }
-            args.Title = & m[2]
-            fn( w, r, m[1], m[2], args )
+			http.FileServer( http.Dir( *args.WebRoot ) )
+			// if debug { fmt.Println( r.URL.Path, patharray ) }
+            // if patharray == nil {
+            //     http.FileServer( http.Dir( *args.WebRoot ) )
+            //     return
+            // }
+            // args.Title = & patharray[2]
+            // fn( w, r, patharray[1], m[2], args )
         }
     }
 }
@@ -337,14 +395,19 @@ func WrapHeadAndFoot( filename, html string ) string {
 	return fmt.Sprintf( "<!DOCTYPE html>\n<html>\n\n<title>\n   %s\n</title>\n<body>\n %s\n</body>\n</html>\n", filename, html )
 }
 
+func WrapPre( filename, html string ) template.HTML {
+	return template.HTML( fmt.Sprintf( "<!DOCTYPE html>\n<html>\n\n<title>\n   %s\n</title>\n<body><pre>\n %s\n</pre></body>\n</html>\n", filename, html ) )
+}
+
 func WikiHandler( w http.ResponseWriter, r *http.Request, path string, filename string, args Arglist ) {
 	if IsWiki( filename ) {
         text := Load( filename )
-		if t, err := wiki.Transform( *text ); err != nil {
-			*text = ""
-
-		} else {
-			*text = WrapHeadAndFoot( filename, t )
+		if text != nil {
+			if t, err := wiki.Transform( *text ); err != nil {
+				*text = ""
+			} else {
+				*text = WrapHeadAndFoot( filename, t )
+			}
 		}
 		w.Write( []byte( template.HTML( *text ) ) )
 	}
@@ -352,11 +415,15 @@ func WikiHandler( w http.ResponseWriter, r *http.Request, path string, filename 
 
 func Handler( w http.ResponseWriter, r *http.Request, path string, filename string, args Arglist ) {
 	text := ""
+	if debug { fmt.Println( filename ) }
 	if IsMarkdown( filename ) {
+		if debug { fmt.Println( "Handler IsMarkdown",  filename ) }
 		text = MarkupMarkdown( filename, args, *args.TemplateName == "plain"  )
 	} else {
 		t := Load( filename )
-		text = *t 
+		if t != nil {
+			text = *t 
+		}
 	}
     if text == "" {
         http.NotFound( w, r )
